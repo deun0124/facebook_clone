f2b363ea18893605f4938db305717018f2b0263d
deun_0124 <m3g3ab@gmail.com>
2021-06-23 09:13:50 +0900

    header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDMSfY5DZbhaSzFixUtblnvbwk2WiMp5so",
+    authDomain: "facebook-clone-9a6a8.firebaseapp.com",
+    projectId: "facebook-clone-9a6a8",
+    storageBucket: "facebook-clone-9a6a8.appspot.com",
+    messagingSenderId: "417160476774",
+    appId: "1:417160476774:web:2f03129671ac2c9b0a55a7",
+    measurementId: "G-DYK39WJ6P3"
+};
